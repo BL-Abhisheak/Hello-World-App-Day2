4442c0f691f8902a389324ac1d66b3115d45f6d5
BL-Abhisheak <abhisheak.bhaskaran@bridgelabz.com>
2026-01-22 12:57:54 +0530

    Code with loggers

--- Day2_Hello_World/Day2_Hello_World/src/main/java/com/hello/Day2HelloWorldApplication.java
+++ Day2_Hello_World/Day2_Hello_World/src/main/java/com/hello/Day2HelloWorldApplication.java
@@ -1,13 +1,23 @@
 package com.hello;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class Day2HelloWorldApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Day2HelloWorldApplication.class, args);
-	}
+    public static final Logger logger =
+            LoggerFactory.getLogger(Day2HelloWorldApplication.class);
 
+    public static void main(String[] args) {
+
+        ApplicationContext context =
+                SpringApplication.run(Day2HelloWorldApplication.class, args);
+
+        logger.info("Welcome to Spring Boot Hello World Application");
+        logger.info("Application Context: " + context);
+    }
 }
