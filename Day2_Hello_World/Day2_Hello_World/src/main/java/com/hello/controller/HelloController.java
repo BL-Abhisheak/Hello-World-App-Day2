c4da5cf4e48c424ddea26a28b469a0df06db6646
BL-Abhisheak <abhisheak.bhaskaran@bridgelabz.com>
2026-01-22 16:24:27 +0530

    Code using pathvariable

--- Day2_Hello_World/Day2_Hello_World/src/main/java/com/hello/controller/HelloController.java
+++ Day2_Hello_World/Day2_Hello_World/src/main/java/com/hello/controller/HelloController.java
@@ -2,6 +2,7 @@ package com.hello.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,6 +13,11 @@ public class HelloController {
         return "Hello from Bridgelabz...";
     }
 
+    @GetMapping("/hello/{name}")
+    public String helloName(@PathVariable String name){
+        return "Hello " + name + " from Bridgelabz...";
+
+    }
 
     // @GetMapping("/web/hello")
     // public String webhello(){
