5001872f012cf15d313ee975bd1e04734232c5bf
BL-Abhisheak <abhisheak.bhaskaran@bridgelabz.com>
2026-01-22 12:38:49 +0530

    Code for REST call display

--- Day2_Hello_World/Day2_Hello_World/src/main/java/com/hello/controller/HelloController.java
+++ Day2_Hello_World/Day2_Hello_World/src/main/java/com/hello/controller/HelloController.java
@@ -6,14 +6,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HelloController {
     
-    // @GetMapping("/hello")
-    // public String hello(){
-    //     return "Hello World";
-    // }
-
-
-    @GetMapping("/web/hello")
-    public String webhello(){
-        return "hello";
+    @GetMapping("/hello")
+    public String hello(){
+        return "Hello from Bridgelabz...";
     }
+
+
+    // @GetMapping("/web/hello")
+    // public String webhello(){
+    //     return "hello";
+    // }
 }
